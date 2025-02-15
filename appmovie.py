13f6a507688c1716c9ad5f558fb37ebaefad5512
Daniela Chaves <>
2025-02-16 00:02:47 +0100

    almost last changes

--- appmovie.py
+++ appmovie.py
@@ -6,23 +6,21 @@ mdata = load_data()
 mdata = preprocess_data(mdata)
 similarity_matrix, tfidf = create_similarity_engine(mdata)
 
-st.set_page_config(
-    page_title= "Dani's Cinema"
-)
-
-def run():
-    img1= Image.open('/Users/hectorchaves/Documents/GitHub/Scientific-Programming---Project/IMG_0116.PNG')
-    st.image(img1, use_row_width= True)
-    st.title("Dani's Cinema")
+img1 = Image.open('/Users/hectorchaves/Documents/GitHub/Scientific-Programming---Project/images/IMG_0116.PNG')
+st.image(img1, use_container_width=True)
+st.title("Dani's Cinema")
 
 movie_title = st.text_input("Enter a movie title:")
 
+# adding a slider for the number of recommendations that the user wants
+num_recommendations = st.slider("Number of Recommendations", min_value=1, max_value=20, value=10, step=1)  # Default 10
+
 if st.button("Get Recommendations"):
     if movie_title:
-        recommendations = recommend_movies(movie_title)
+        recommendations = recommend_movies(movie_title, top_n=num_recommendations)  # Pass num_recommendations to the function
         st.write("Recommendations:")
         if recommendations[0] == "Movie not found.":
-            st.write(recommendations[0]) #print the error
+            st.write(recommendations[0])
         else:
             for movie in recommendations:
                 st.write(movie)
